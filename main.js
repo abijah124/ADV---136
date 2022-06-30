2327a1fb5c314c4fc4544a7cd1bd00047ecea43b
abijah124 <108469459+abijah124@users.noreply.github.com>
2022-06-30 21:58:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+status = "";
+value = "";
+function setup(){
+canvas = createCanvas(480, 380);
+canvas.center();
+video = createCapture(VIDEO);
+video.size(480, 380);
+video.hide();
+}
+function start(){
+objectDetector = ml5.objectDetector('cocossd',modeloaded);
+document.getElementById("status").innerHTML = "Status : object Detected";
+value = document.getElementById("object_input").value;
+}
+function modeloaded(){
+console.log("Model loaded");
+status = true;
+}
+function draw(){
+image(video,0,0,480,380);
+ }
